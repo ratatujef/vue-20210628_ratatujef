31b81dc3d460cbea02909b75328354bfe333b460
Ivan Glebov <iglebov@kameleoon.com>
2021-07-19 20:10:58 +0300

    methods

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
+const app = createApp({
+  data() {
+    return {
+      total: 0,
+      firstNum: 0,
+      secondNum: 0,
+      func: '',
+    };
+  },
+  updated() {
+    this.calculate();
+  },
+  methods: {
+    calculate() {
+      switch (this.func) {
+        case 'sum':
+          this.total = this.firstNum + this.secondNum;
+          break;
+        case 'subtract':
+          this.total = +this.firstNum - this.secondNum;
+          break;
+        case 'multiply':
+          this.total = this.firstNum * this.secondNum;
+          break;
+        case 'divide':
+          this.total = this.firstNum / this.secondNum;
+          break;
+      }
+    },
+  },
+}).mount('#app');
 // Создайте Vue приложение
