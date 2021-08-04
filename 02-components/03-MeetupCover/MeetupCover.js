8b4d0ce178308dc221b737a0946cc81f42c9a13c
Ivan Glebov <iglebov@kameleoon.com>
2021-08-04 15:56:26 +0300

    working, but ot tested

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -2,9 +2,17 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
-
+  props: {
+    image: String,
+    title: String,
+  },
+  computed: {
+    background() {
+      return `--bg-url: url(${this.image || 'https://course-vue.javascript.ru/api/images/2'})`;
+    },
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="background">
+        <h1 v-if='title' class="meetup-cover__title">{{title}}</h1>
     </div>`,
 });
