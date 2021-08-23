63bc6f30dcd0cb0556e624d43e963a405e3bd5c1
Ivan Glebov <iglebov@kameleoon.com>
2021-08-23 20:54:52 +0300

    meetupCover_V-bind

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    image: String,
+    title: String,
+  },
+  computed: {
+    bg() {
+      return ` linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(${this.image || '--default-cover'})`;
+    },
+  },
 };
 </script>
 
@@ -16,7 +25,8 @@ export default {
   background-position: center;
   background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
     url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: v-bind(bg);
+  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover);  */
   display: flex;
   flex-direction: column;
   align-items: center;
