c2c8d5588d583382076499198479b11cbff8c508
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-03-18 09:26:55 +0300

    Added task Потеря реактивности

---
+++ 08-reactivity/01-reactivity-losing/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ui-container class="sample">
+    <sensors-data-container />
+  </ui-container>
+</template>
+
+<script>
+import UiContainer from './components/UiContainer';
+import SensorsDataContainer from './components/SensorsDataView';
+
+export default {
+  name: 'App',
+
+  components: { SensorsDataContainer, UiContainer },
+};
+</script>
+
+<style></style>
