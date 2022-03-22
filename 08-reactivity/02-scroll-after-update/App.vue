8d582b157419308cffe71b0f53a830597643f22f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-03-22 16:04:14 +0300

    Added task Скроллинг после обновления

---
+++ 08-reactivity/02-scroll-after-update/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <div class="sample container">
+      <mini-messenger />
+    </div>
+  </div>
+</template>
+
+<script>
+import MiniMessenger from './components/MiniMessenger';
+
+export default {
+  name: 'App',
+  components: { MiniMessenger },
+};
+</script>
+
+<style></style>
