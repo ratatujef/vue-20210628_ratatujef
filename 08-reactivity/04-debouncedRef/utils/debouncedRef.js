6f8a87d3f2883ec8810191dda80b73d612e371a4
Ivan Glebov <iglebov@kameleoon.com>
2022-03-23 17:29:21 +0300

    debounce

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -7,8 +7,11 @@ import debounce from 'lodash/debounce';
  * @param {number} wait - Ожидание в миллисекундах для debounce
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
+
 export function debouncedRef(source, wait) {
-  const debounced = ref(undefined); // ...
-  // ...
+  const debounced = ref(source.value); // ...
+  const updateValue = debounce((newVal) => (debounced.value = newVal), wait);
+  watch(source, () => updateValue(source.value));
+
   return debounced;
 }
