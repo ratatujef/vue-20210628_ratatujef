d896e6b004a3d79d3139d4e0ac4753b2a8033931
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-04-11 12:48:33 +0300

    Added task VNode

---
+++ 09-rendering/01-VNode/components/VNode.vue
@@ -0,0 +1,15 @@
+<script>
+import { isVNode } from 'vue';
+
+export default {
+  name: 'VNode',
+
+  props: {
+    vnode: {
+      type: [Object, Array],
+      require: true,
+      validator: (vnodes) => (Array.isArray(vnodes) ? vnodes : [vnodes]).every((mustBeVNode) => isVNode(mustBeVNode)),
+    },
+  },
+};
+</script>
---
+++ 09-rendering/01-VNode/main.js
@@ -0,0 +1,8 @@
+import '@/assets/styles/taskbook.css';
+import '@/assets/styles/app.css';
+import '@/assets/styles/_container.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
